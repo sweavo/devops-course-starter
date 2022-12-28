b50e22d4794f8e42f7209691a3a6cc3275c60e9f
Steve Carter <sweavo@gmail.com>
2022-12-28 14:53:53 +0000

    form can cause the POST action

--- todo_app/app.py
+++ todo_app/app.py
@@ -16,6 +16,6 @@ def index():
     todo_items = session_items.get_items()
     return flask.render_template('index.html',items=todo_items)
 
-@app.route('/additem', methods=['POST'] )
+@app.route('/additem', methods=['POST'])
 def additem():
-    return flask.make_response("Add not implemented",501) //TODO
+    return flask.make_response("Add not implemented",501) #TODO
