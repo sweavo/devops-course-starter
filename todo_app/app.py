6e62a5a0e79e5514c1dfd3464762ea1b0d7203b0
Steve Carter <steve.carter@etas.com>
2023-01-10 10:08:31 +0000

    exploration of errors. Start at URL /error

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,11 +1,23 @@
 import flask
+import http
+import werkzeug.exceptions as wz_exceptions
 
 from todo_app.flask_config import Config
 
 from todo_app.data import session_items 
 
+
 app = flask.Flask(__name__)
 app.config.from_object(Config())
+
+def error_menu():
+    """ Generator function to make a clickable menu of status codes"""
+    yield "<html><head><title>Error menu</title></head><body><ul>"
+    for code in http.HTTPStatus:
+        is_exception = code in wz_exceptions.default_exceptions
+        flask_api = ['response','abort'][is_exception]
+        yield f"<li><a href='/error/{code.value}'>flask.{flask_api}({code.value})</a>: {code.description or code.name}</li>"
+    yield "</ul></body></html>"
 
 @app.route('/test')
 def test():
@@ -15,6 +27,19 @@ def test():
 def index():
     todo_items = session_items.get_items()
     return flask.render_template('index.html',items=todo_items)
+
+@app.route('/error')
+@app.route('/error/<which>')
+def return_error(which=None):
+    if which:
+        status=int(which)
+        if status in wz_exceptions.default_exceptions:
+            flask.abort(status)
+        else:
+            return flask.Response(status=status)
+    else:
+        return ''.join(error_menu())
+
 
 @app.route('/additem', methods=['POST'])
 def additem():
