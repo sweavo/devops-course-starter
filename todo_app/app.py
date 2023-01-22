99d845099f763e226b3d831cc9f17caf4011a2e0
Steve Carter <sweavo@gmail.com>
2023-01-22 08:42:44 +0000

    rename the module for taste

--- todo_app/app.py
+++ todo_app/app.py
@@ -2,7 +2,8 @@ import flask
 
 from todo_app.flask_config import Config
 
-from todo_app.data import storage_trello as session_items
+from todo_app.data import storage_trello as persistence
+
 
 app = flask.Flask(__name__)
 app.config.from_object(Config())
@@ -13,20 +14,20 @@ def test():
 
 @app.route('/')
 def index():
-    todo_items = session_items.get_items()
+    todo_items = persistence.get_items()
     return flask.render_template('index.html',items=todo_items)
 
 @app.route('/additem', methods=['POST'])
 def add_item():
     item_title = flask.request.form.get('title')
     if item_title: # don't add empty items
-        session_items.add_item(item_title)
+        persistence.add_item(item_title)
     return flask.redirect('/') # discourage revisiting this URL
 
 @app.route('/completeitem/<id>', methods=['POST'])
 def complete_item(id):
-    card = session_items.get_item(id)
-    card['status'] = 'Done'
-    session_items.save_item(card)
+    card = persistence.get_item(id)
+    card.status = 'Done'
+    persistence.save_item(card)
     return flask.redirect('/') # discourage revisiting this URL
 
