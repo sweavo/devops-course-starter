4dd3e17035430ee06496f605242b2921eabf7342
Steve Carter <steve.carter@etas.com>
2023-03-07 11:25:35 +0000

    failing test for todo items

--- tests/test_view_model.py
+++ tests/test_view_model.py
@@ -22,3 +22,21 @@ def test_get_done_items():
 
     assert B in done_items
     assert D in done_items
+
+
+def test_get_todo_items():
+    """! This tests assumes what persistence layer will return into ViewModel"""
+    A, B, C, D = (storage_trello.Card(1, 'one', 'Not Started'),
+                  storage_trello.Card(2, 'two', 'Done'),
+                  storage_trello.Card(3, 'three', 'Not Started'),
+                  storage_trello.Card(4, 'four', 'Done'))
+
+    viewmodel_under_test = ViewModel([A, B, C, D])
+
+    done_items = viewmodel_under_test.todo_items
+
+    assert A in done_items
+    assert C in done_items
+
+    assert B not in done_items
+    assert D not in done_items
