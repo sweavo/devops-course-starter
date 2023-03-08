cf268306c47719d8680f8679e54c598b07878edb
Steve Carter <steve.carter@etas.com>
2023-03-08 17:15:17 +0000

    test now correctly failing

--- tests/integration_test.py
+++ tests/integration_test.py
@@ -1,15 +1,22 @@
 import pytest
+import sys
 
-import dotenv
+from dotenv import find_dotenv, load_dotenv
 from todo_app import app
 
 
 @pytest.fixture
 def client():
-    file_path = dotenv.find_dotenv('.env.test')
-    dotenv.load_dotenv(file_path, override=True)
+    file_path = find_dotenv('.env.test')
+
+    load_dotenv(file_path, override=True)
 
     test_app = app.create_app()
 
     with test_app.test_client() as client:
         yield client
+
+
+def test_index_page(client):
+    response = client.get('/')
+    print(response.data, file=sys.stderr)
