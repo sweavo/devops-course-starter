50a56e81e8452f95a9abc93ae5b1e9425d0edf5b
Steve Carter <steve.carter@etas.com>
2023-03-08 17:36:31 +0000

    going down a rabbithole with integration test. need seams

--- tests/integration_test.py
+++ tests/integration_test.py
@@ -1,8 +1,11 @@
+import re
 import pytest
-import sys
+import requests
 
 from dotenv import find_dotenv, load_dotenv
 from todo_app import app
+
+RE_API = re.compile('^https://api.trello.com/1/boards?/[a-z0-9]+/lists')
 
 
 @pytest.fixture
@@ -17,6 +20,32 @@ def client():
         yield client
 
 
-def test_index_page(client):
+def test_index_page(monkeypatch, client):
+    # This replaces any call to requests.get with our own function
+    monkeypatch.setattr(requests, 'get', stub)
+
     response = client.get('/')
-    print(response.data, file=sys.stderr)
+
+    assert 'hello' in response.data
+
+
+class StubResponse():
+    def __init__(self, fake_response_data):
+        self.fake_response_data = fake_response_data
+        self.status_code = 200
+
+    def json(self):
+        return self.fake_response_data
+
+
+def stub(url, params={}, proxies=None, verify=None):
+    m = RE_API.match(url)
+    if m:
+        fake_response_data = [{
+            'id': '123abc',
+            'name': 'To Do',
+            'cards': [{'id': '456', 'name': 'Test card'}]
+        }]
+        return StubResponse(fake_response_data)
+
+    raise Exception(f'Integration test did not expect URL "{url}"')
