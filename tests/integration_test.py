3898bc60710adb6d8c1036569364dac722834df2
Steve Carter <steve.carter@etas.com>
2023-03-13 19:39:43 +0000

    First integration test is working.  Points to some coupling

--- tests/integration_test.py
+++ tests/integration_test.py
@@ -1,32 +1,13 @@
-import re
+import json
 import pytest
+import re
 import requests
 
 from dotenv import find_dotenv, load_dotenv
 from todo_app import app
 
+# How to recognize an incoming request for the boards, regardless of the authentication given
 RE_API = re.compile('^https://api.trello.com/1/boards?/[a-z0-9]+/lists')
-
-
-@pytest.fixture
-def client():
-    file_path = find_dotenv('.env.test')
-
-    load_dotenv(file_path, override=True)
-
-    test_app = app.create_app()
-
-    with test_app.test_client() as client:
-        yield client
-
-
-def test_index_page(monkeypatch, client):
-    # This replaces any call to requests.get with our own function
-    monkeypatch.setattr(requests, 'get', stub)
-
-    response = client.get('/')
-
-    assert 'hello' in response.data
 
 
 class StubResponse():
@@ -38,14 +19,33 @@ class StubResponse():
         return self.fake_response_data
 
 
-def stub(url, params={}, proxies=None, verify=None):
+def stub_requests_get(url, params={}, proxies=None, verify=None):
     m = RE_API.match(url)
     if m:
-        fake_response_data = [{
-            'id': '123abc',
-            'name': 'To Do',
-            'cards': [{'id': '456', 'name': 'Test card'}]
-        }]
-        return StubResponse(fake_response_data)
+        with open("tests/test_data/board_result.json", "r") as fp:
+            board_json = json.load(fp)
+        return StubResponse(board_json)
 
     raise Exception(f'Integration test did not expect URL "{url}"')
+
+
+@pytest.fixture
+def client():
+    """Fixture to connect networklessly to the app"""
+
+    file_path = find_dotenv('.env.test')
+
+    load_dotenv(file_path, override=True)
+
+    test_app = app.create_app()
+
+    with test_app.test_client() as client:
+        yield client
+
+
+def test_index_page(monkeypatch, client):
+    monkeypatch.setattr(requests, 'get', stub_requests_get)
+    response = client.get('/')
+
+    assert 200 == response.status_code
+    assert 'second item' in response.data.decode()
